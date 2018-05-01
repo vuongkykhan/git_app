44cf09753434df2c81534b7d6b33e7629e1b3a95
vuongkykhan <vuongkykhan@gmail.com>
2018-05-01 15:23:08 +0700

    Fix test2

--- app/controllers/welcome_controller_controller.rb
+++ app/controllers/welcome_controller_controller.rb
@@ -1,12 +1,7 @@
 class WelcomeControllerController < ApplicationController
   def index; end
-<<<<<<< HEAD
   def contact'; end
   def about; end
-=======
-
   def about; end
-
->>>>>>> master
   def help; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   root to: "welcome_controller#index"
   get "/about", to: "welcome_controller#about"
-<<<<<<< HEAD
   get "/contact", to: "welcome_controller#contact"
   get "/help", to: "welcome_controller#help"
-=======
-
   get "/help", to: "welcome_controller#help"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> master
 end
