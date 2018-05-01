93315774a00ffbfcd8a81348d30fd17fef0305ff
vuongkykhan <vuongkykhan@gmail.com>
2018-05-01 15:21:04 +0700

    test2

--- app/controllers/welcome_controller_controller.rb
+++ app/controllers/welcome_controller_controller.rb
@@ -1,7 +1,12 @@
 class WelcomeControllerController < ApplicationController
   def index; end
+<<<<<<< HEAD
+  def contact'; end
+  def about; end
+=======
 
   def about; end
 
+>>>>>>> master
   def help; end
 end
